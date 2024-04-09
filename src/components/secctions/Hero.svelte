9aa6682e07aaf3805fc6323d921fd60b30c264bb
bryanby2002 <brayan7br7@gmail.com>
2024-04-09 11:08:00 -0500

    add icon email anf completed component Hero

--- src/components/secctions/Hero.svelte
+++ src/components/secctions/Hero.svelte
@@ -1,15 +1,13 @@
 <script>
-	import GitHub from './../icons/GitHub.svelte';
 
     import Layout from "./../Layout.svelte";
     import { basics } from "./../../models/cv.json";
-    import GithubIcon from './../icons/GitHub.svelte'
-    import LinkedinIcon from './../icons/LInkedin.svelte'
-
+    import GithubIcon from "./../icons/GitHub.svelte";
+    import LinkedinIcon from "./../icons/LInkedin.svelte";
+    import Email from "../icons/Email.svelte";
 
     const { label, name, lastName, location, phone, email, img } = basics;
     const { city, countryCode, region } = location;
-
 </script>
 
 <Layout title="">
@@ -19,24 +17,35 @@
                 {name}
                 {lastName}
             </h2>
-            <p class="text-slate-700 font-bold ">{label}</p>
+            <p class="text-slate-700 font-bold">{label}</p>
             <div class="flex gap-2 mt-1">
                 <p class="text-sm text-slate-600">
                     {city}, {region}, {countryCode} |
                 </p>
-                <p class="text-sm text-slate-600">{phone} |</p>
-                <a
-                    class="text-sm text-slate-600"
-                    target="_blank"
-                    title="Envia un mensaje a {email}"
-                    href="mailto:brayan7br7@gmail.com"
-                >
-                    {email}
-                </a>
+                <p class="text-sm text-slate-600">{phone} </p>
             </div>
-            <div class="flex text-slate-600">
-                <GithubIcon/>
-                <LinkedinIcon/>
+            <div class="flex text-slate-600 gap-1">
+                <a
+                    href="https://github.com/bryanby2002?tab=repositories"
+                    target="_blank"
+                    class="p-2 border rounded hover:bg-slate-200"
+                >
+                    <GithubIcon />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/bryanby2002/"
+                    target="_blank"
+                    class="p-2 border rounded hover:bg-slate-200"
+                >
+                    <LinkedinIcon />
+                </a>
+                <a
+                    href="mailto:brayan7br7@gmail.com"
+                    target="_blank"
+                    class="p-2 border rounded hover:bg-slate-200"
+                >
+                    <Email />
+                </a>
             </div>
         </div>
         <div>
