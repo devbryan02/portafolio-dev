b6cb2986b41030e699037692784876e21a6d7e17
bryanby2002 <brayan7br7@gmail.com>
2024-04-10 00:11:42 -0500

    remove icons

--- src/components/secctions/Skil.svelte
+++ src/components/secctions/Skil.svelte
@@ -1,10 +1,19 @@
 <script>
-    import Layout from "../Layout.svelte";
+	import GitHub from './../icons/GitHub.svelte';
+	import Git from './../icons/Git.svelte';
+	import Tailwind from './../icons/Tailwind.svelte';
+	import Css from './../icons/Css.svelte';
+	import Mysql from './../icons/Mysql.svelte';
+	import Html from './../icons/Html.svelte';
+	import Spring from './../icons/Spring.svelte';
+    import Java from "../icons/Java.svelte";
     import {skills} from './../../models/cv.json'
+    import Layout from "../Layout.svelte";
+    
 </script>
 
 <Layout title="Habilidades">
-    <div class="flex gap-2 my-5 flex-wrap">
+    <div class="flex justify-between my-5 flex-wrap">
         {#each skills as skill}
             <span class="text-sm text-slate-600 px-2 py-1 bg-gray-200 rounded cursor-pointer">
                 {skill.name}
