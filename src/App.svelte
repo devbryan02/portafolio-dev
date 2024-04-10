e47bc0649705ba416e17e5aa78a4817327766688
bryanby2002 <brayan7br7@gmail.com>
2024-04-09 23:02:15 -0500

    completed education component

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,13 @@
 <script>
-
-    import Hero from "./components/secctions/Hero.svelte";
-    import About from "./components/secctions/About.svelte";
-    import Project from "./components/secctions/Project.svelte";
-    
+  import Hero from "./components/secctions/Hero.svelte";
+  import About from "./components/secctions/About.svelte";
+  import Project from "./components/secctions/Project.svelte";
+  import Education from "./components/secctions/Education.svelte";
 </script>
 
 <main>
-
-    <Hero/>
-    <About/>
-    <Project/>
-
+  <Hero />
+  <About />
+  <Project />
+  <Education/>
 </main>
---
+++ src/components/secctions/Education.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {education} from './../../models/cv.json'
+    import Layout from '../Layout.svelte';
+
+    const {instition, area, endDate, startDate, url}  = education
+</script>
+
+<Layout title="Education">
+    <div class="flex justify-between mt-5">
+        <p class="text-sm text-slate-700 font-bold">{instition}</p>
+        <p class="text-sm text-slate-500">{startDate} - {endDate}</p>
+    </div>
+    <p class="text-sm text-slate-600">{area}</p>
+</Layout>
+
